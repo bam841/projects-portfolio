eaee6c9c38477a22fcb7a65b1cdf2ed48e2b644c
JOHN ABRAHM A. ZAPICO <zapicoja@students.nu-Lipa.edu.ph>
2025-09-02 08:17:41 +0800

    Update dataStructuresMidterms.cpp

--- dataStructuresMidterms.cpp
+++ dataStructuresMidterms.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <algorithm> // Needed for swap in bubbleSort
 using namespace std;
- 
+
 class ListSystem {
 private:
     int* data;
     int n;
     int MAX;
- 
+
 public:
     // Constructor to initialize the list with a given max size
     ListSystem(int max_size) {
@@ -15,12 +15,12 @@ public:
         data = new int[MAX];
         n = 0;  // Initially, the list is empty
     }
- 
+
     // Destructor to free the allocated memory
     ~ListSystem() {
         delete[] data;
     }
- 
+
     // Append - Adds a record to the end of the list
     void append(int value) {
         if (n == MAX) {
@@ -31,7 +31,7 @@ public:
             cout << "Append successful" << endl;
         }
     }
- 
+
     // Replace - Replace a value at index k with new value
     void replace(int k, int newValue) {
         if (k < 0 || k >= n) {
@@ -41,7 +41,7 @@ public:
             cout << "Replacement successful" << endl;
         }
     }
- 
+
     // Insert - Inserts a value at position pos
     void insert(int pos, int value) {
         if (n == MAX) {
@@ -57,7 +57,7 @@ public:
             cout << "Insertion successful" << endl;
         }
     }
- 
+
     // Delete - Deletes a value at position pos
     void deleteAt(int pos) {
         if (n == 0) {
@@ -72,7 +72,7 @@ public:
             cout << "Deletion successful" << endl;
         }
     }
- 
+
     // Search - Search for an element in the list
     int search(int key) {
         for (int i = 0; i < n; i++) {
@@ -82,7 +82,7 @@ public:
         }
         return -1; // Not found
     }
- 
+
     // BubbleSort - Sorts the list in ascending order
     void bubbleSort() {
         for (int i = 0; i < n - 1; i++) {
@@ -94,121 +94,120 @@ public:
         }
         cout << "Sort successful" << endl;
     }
- 
-    // Catenate - Concatenates another list to the current list
-    void catenate(int* otherList, int m) {
-        if (n + m > MAX) {
-            cout << "Error: Not enough space to concatenate" << endl;
-        } else {
-            for (int i = 0; i < m; i++) {
-                data[n] = otherList[i];
-                n++;
-            }
-            cout << "Catenation successful" << endl;
+
+    // Export array data (for catenation/merge)
+    void exportArray(int* arr) {
+        for (int i = 0; i < n; i++) {
+            arr[i] = data[i];
         }
     }
- 
-    // Merge - Merges two sorted lists
-    void merge(int* otherList, int m) {
-        int i = 0, j = 0, k = 0;
-        // Make sure merged size won't exceed MAX
-        if (n + m > MAX) {
-            cout << "Error: Not enough space to merge" << endl;
-            return;
-        }
-        int* mergedList = new int[n + m];
- 
-        // Merge the two lists
-        while (i < n && j < m) {
-            if (data[i] <= otherList[j]) {
-                mergedList[k++] = data[i++];
-            } else {
-                mergedList[k++] = otherList[j++];
-            }
-        }
- 
-        // Copy remaining elements
-        while (i < n) {
-            mergedList[k++] = data[i++];
-        }
-        while (j < m) {
-            mergedList[k++] = otherList[j++];
-        }
- 
-        // Copy merged list back to data
-        delete[] data;
-        data = mergedList;
-        n = k;
- 
-        cout << "Merge successful" << endl;
-    }
- 
+
     // Display the current list
-    void display() {
+    void display(const char* name = "Array") {
+        cout << name << ": ";
         for (int i = 0; i < n; i++) {
             cout << data[i] << " ";
         }
         cout << endl;
     }
- 
+
     // Get the current size of the list
     int size() {
         return n;
     }
+
+    // Set from external array
+    void setFromArray(const int* arr, int arrSize) {
+        n = arrSize;
+        for (int i = 0; i < n; i++) {
+            data[i] = arr[i];
+        }
+    }
+
+    // Catenate arr1 and arr2 into this list (for C)
+    void catenate(const int* arr1, int n1, const int* arr2, int n2) {
+        if (n1 + n2 > MAX) {
+            cout << "Error: Not enough space to concatenate" << endl;
+            n = 0;
+            return;
+        }
+        n = 0;
+        for (int i = 0; i < n1; i++) data[n++] = arr1[i];
+        for (int i = 0; i < n2; i++) data[n++] = arr2[i];
+        cout << "Catenation successful" << endl;
+    }
+
+    // Merge two sorted arrays into this list (for C)
+    void merge(const int* arr1, int n1, const int* arr2, int n2) {
+        if (n1 + n2 > MAX) {
+            cout << "Error: Not enough space to merge" << endl;
+            n = 0;
+            return;
+        }
+        int i = 0, j = 0;
+        n = 0;
+        while (i < n1 && j < n2) {
+            if (arr1[i] <= arr2[j]) data[n++] = arr1[i++];
+            else data[n++] = arr2[j++];
+        }
+        while (i < n1) data[n++] = arr1[i++];
+        while (j < n2) data[n++] = arr2[j++];
+        cout << "Merge successful" << endl;
+    }
 };
- 
-// Example usage
+
 int main() {
-    ListSystem system(10); // Max size of 10
- 
-    // Append some elements
-    system.append(10);
-    system.append(20);
-    system.append(30);
- 
-    // Display current list
-    cout << "Current list: ";
-    system.display();
- 
-    // Replace an element
-    system.replace(1, 25);
-    cout << "After replacing index 1: ";
-    system.display();
- 
-    // Insert an element
-    system.insert(1, 15);
-    cout << "After inserting 15 at index 1: ";
-    system.display();
- 
-    // Delete an element
-    system.deleteAt(2);
-    cout << "After deleting index 2: ";
-    system.display();
- 
-    // Search for an element
-    int index = system.search(25);
-    cout << "Index of 25: ";
-    if (index != -1)
-        cout << index << endl;
-    else
-        cout << "Not found" << endl;
- 
-    // Sort the list
-    system.bubbleSort();
-    cout << "After sorting: ";
-    system.display();
- 
-    // Catenate two lists
-    int otherList[] = {40, 50};
-    system.catenate(otherList, 2);
-    cout << "After concatenating [40, 50]: ";
-    system.display();
- 
-    // Merge two lists
-    int listToMerge[] = {5, 60};
-    system.merge(listToMerge, 2);
-    cout << "After merging with [5, 60]: ";
-    system.display();
- 
+    // Create three arrays
+    ListSystem A(10); // Array A
+    ListSystem B(10); // Array B
+    ListSystem C(20); // Array C (for catenate/merge results)
+
+    // Add elements to A
+    A.append(10);
+    A.append(20);
+    A.append(30);
+    cout << "Initial ";
+    A.display("A");
+
+    // Add elements to B
+    B.append(15);
+    B.append(25);
+    cout << "Initial ";
+    B.display("B");
+
+    // Demonstrate basic operations on A
+    A.replace(1, 22); // Replace index 1
+    A.display("A after replace");
+
+    A.insert(1, 12); // Insert at index 1
+    A.display("A after insert");
+
+    A.deleteAt(2); // Delete index 2
+    A.display("A after delete");
+
+    int idx = A.search(22);
+    cout << "Index of 22 in A: ";
+    if (idx != -1) cout << idx << endl;
+    else cout << "Not found" << endl;
+
+    A.bubbleSort();
+    A.display("A after sort");
+
+    // Catenate A and B into C
+    int arrA[10], arrB[10];
+    int sizeA = A.size(), sizeB = B.size();
+    A.exportArray(arrA);
+    B.exportArray(arrB);
+    C.catenate(arrA, sizeA, arrB, sizeB);
+    C.display("C = A + B (catenate)");
+
+    // Merge A and B into C (both sorted)
+    A.bubbleSort();
+    B.bubbleSort();
+    A.exportArray(arrA);
+    B.exportArray(arrB);
+    C.merge(arrA, sizeA, arrB, sizeB);
+    C.display("C = merge(A, B)");
+
     return 0;
 }
